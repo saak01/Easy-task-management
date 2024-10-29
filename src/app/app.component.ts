0f33f748814a19a553e4a9658346119ebc015ee1
Joao Victor Alves Costa <joao.victor@sysout.com.br>
2024-10-29 18:47:17 -0300

    user component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
+import { UserComponent } from "./pages/user/user.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/pages/user/user.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../../../db/dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.scss'
+})
+export class UserComponent {
+
+  selectedUser = DUMMY_USERS[randomIndex];
+
+  get imagePath() {
+    return `assets/users/${this.selectedUser.avatar}`
+  }
+}
